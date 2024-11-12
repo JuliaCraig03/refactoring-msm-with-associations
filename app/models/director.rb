bb09f50baaa7d8924f78cda8c2dae78807471321
JuliaCraig03 <juliacraig@uchicago.edu>
2024-11-12 23:51:35 +0000

    83.84%

--- app/models/director.rb
+++ app/models/director.rb
@@ -12,4 +12,7 @@
 #
 class Director < ApplicationRecord
   validates(:name, presence: true)
+  belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
+  has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
+  has_many(:cast, :through => :characters, :source => :actor)
 end
